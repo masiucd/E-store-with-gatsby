304adf1f1a3c572135376dafba4540ac8f8351b0
marcell <ciszekmarcell@gmail.com>
2019-03-13 13:30:02 +0100

    react setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './index.html';
+
+const App = () => <h1>It works</h1>;
+
+ReactDOM.render(<App />, document.querySelector('#app'));
