a02bd544167b30f17c949f2fa9fb63a732280428
marcell <ciszekmarcell@gmail.com>
2019-03-14 08:36:50 +0100

    adding

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import './index.html';
-
-const App = () => <h1>It works</h1>;
-
-ReactDOM.render(<App />, document.querySelector('#app'));
