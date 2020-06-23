63970546145e77d4f3fb7d6e9818f1cb0f8d3583
Masiu <ciszekmarcell@gmail.com>
2020-06-23 16:30:20 +0200

    stripe api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `E-store with Gatsby`,
+    titleTemplate: '%s · E-store',
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@marcellciszek`,
+    twitter: `https://twitter.com/CiszekMarcell`,
   },
   plugins: [
+    'gatsby-plugin-stripe',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -16,6 +22,28 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: [
+          'Balance',
+          'BalanceTransaction',
+          'Product',
+          'ApplicationFee',
+          'Sku',
+          'Subscription',
+          'Price',
+        ],
+        secretKey: process.env.STRIPE_KEY,
+        downloadFiles: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -24,11 +52,12 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/math.png`, // This path is relative to the root of the site.
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
-import { Link, PageProps } from 'gatsby';
+import { Link, PageProps, graphql } from 'gatsby';
 
-const IndexPage: React.FC<PageProps> = () => (
-  <>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </>
-);
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+}
+interface Node {
+  node: Product;
+}
+interface Props {
+  products: {
+    edges: Array<Node>;
+  };
+}
+
+const IndexPage: React.FC<PageProps<Props>> = ({ data }) => {
+  console.log(data.products.edges);
+  return (
+    <>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      {data.products.edges.map(({ node }) => {
+        return <h3 key={node.id}>{node.name}</h3>;
+      })}
+    </>
+  );
+};
+
+export const PAGE_QUERY = graphql`
+  {
+    products: allStripeProduct {
+      edges {
+        node {
+          id
+          name
+          description
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
