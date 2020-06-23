26e9e3e6999ba5d089572275ebf8a70c98acea5f
Masiu <ciszekmarcell@gmail.com>
2020-06-23 17:39:24 +0200

    adding shopify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,20 @@
 import React from 'react';
 import { Link, PageProps, graphql } from 'gatsby';
 
-interface Product {
-  id: string;
-  name: string;
-  description: string;
-}
-interface Node {
-  node: Product;
-}
 interface Props {
   products: {
-    edges: Array<Node>;
+    edges: any;
   };
 }
 
 const IndexPage: React.FC<PageProps<Props>> = ({ data }) => {
-  console.log(data.products.edges);
   return (
     <>
       <h1>Hi people</h1>
       <p>Welcome to your new Gatsby site.</p>
       <p>Now go build something great.</p>
-      {data.products.edges.map(({ node }) => {
-        return <h3 key={node.id}>{node.name}</h3>;
-      })}
     </>
   );
 };
-
-export const PAGE_QUERY = graphql`
-  {
-    products: allStripeProduct {
-      edges {
-        node {
-          id
-          name
-          description
-        }
-      }
-    }
-  }
-`;
 
 export default IndexPage;
