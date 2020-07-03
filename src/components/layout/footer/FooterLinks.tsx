7fa2e8dd27c3be11fc0d59e317ee48ded1c5de99
Masiu <ciszekmarcell@gmail.com>
2020-07-03 18:27:56 +0200

    footer

--- src/components/layout/footer/FooterLinks.tsx
+++ src/components/layout/footer/FooterLinks.tsx
@@ -30,6 +30,29 @@ const LinksList = styled.ul`
     background: none !important;
     a {
       color: ${props => props.theme.colors.text};
+      text-transform: capitalize;
+      position: relative;
+      transition: ${({ theme }) => theme.transition.quickTransition};
+      display: block;
+      &:after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 0%;
+        height: 0;
+        padding: 0;
+        background: ${props => props.theme.colors.secondary};
+        transition: ${({ theme }) => theme.transition.quickTransition};
+      }
+      &:hover {
+        color: ${props => props.theme.colors.secondary};
+        &:after {
+          width: 100%;
+          height: 0.2rem;
+          padding: 0.1rem;
+        }
+      }
     }
   }
 `;
